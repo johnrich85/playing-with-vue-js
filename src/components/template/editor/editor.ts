3e318efd8f9bdaa381f6ae4f5426f01fee11de61
John Richardson <johnrich85@hotmail.com>
2018-01-18 16:32:21 +0000

    Got the mock template up and running. Just need to pair it up with the various block types now.

--- src/components/template/editor/editor.ts
+++ src/components/template/editor/editor.ts
@@ -4,11 +4,13 @@ import Component from 'vue-class-component';
 import SavedTemplate from '../../../models/SavedTemplate';
 import {Prop} from 'vue-property-decorator';
 import {BlockRendererComponent} from '../block-renderer/block-renderer';
+import LineGraph from '../../charts/line';
 
 @Component({
     template: require('./editor.html'),
     components: {
         'v-block-renderer': BlockRendererComponent,
+        'v-line-graph': LineGraph
     }
 })
 
@@ -16,6 +18,18 @@ export class TemplateEditorComponent extends Vue {
 
     @Prop() protected template: SavedTemplate;
 
+    protected tempData: Object = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+            {
+                label: 'Stats',
+                backgroundColor: '#632770',
+                pointBackgroundColor: '#481054',
+                steppedLine : false,
+                data: [40, 39, 10, 40, 39, 80, 40]
+            }
+        ]
+    };
 
     constructor(options?: ComponentOptions<Vue>) {
         super(options);
--- src/data/RemoteKeyInfo.ts
+++ src/data/RemoteKeyInfo.ts
@@ -2,12 +2,24 @@ let remoteKeyinfo = Array<Object>();
 
 remoteKeyinfo.push({
     id : 1,
-    title : 'Automotive sector - general info'
+    title : 'Automotive sector - general info',
+    data : [
+        [
+            {label: 'Growth expected', value: 'Over 80% of businesses reported growth'},
+            {label: 'Shortage of skilled workers', value: 'Since brexit there has been a sharp decline in skilled Automotive workers.'},
+        ],
+    ],
 });
 
 remoteKeyinfo.push({
     id : 2,
-    title : 'Automotive sector - north east info'
+    title : 'Automotive sector - north east info',
+    data : [
+        [
+            {label: 'Growth expected', value: 'Over 80% of businesses reported growth'},
+            {label: 'Shortage of skilled workers', value: 'Since brexit there has been a sharp decline in skilled Automotive workers.'},
+        ],
+    ],
 });
 
 
